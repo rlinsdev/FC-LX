ae57c7f4bef33cb19f5bd54c808397735c92fefa
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-18 14:38:23 -0300

    Chat implementation

--- ChatService/internal/domain/entity/chat.go
+++ ChatService/internal/domain/entity/chat.go
@@ -1,5 +1,7 @@
 package entity
 
+import "errors"
+
 type Chatconfig struct {
 	Model            *Model
 	Temperature      float32  // Precision of answered
@@ -20,4 +22,47 @@ type Chat struct {
 	Status               string
 	TokenUsage           int
 	Config               *Chatconfig
+}
+
+func (c *Chat) Validate() error {
+	if c.UserID == "" {
+			return errors.New(("user id is empty"))
+	}
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is ended. No more message allowed")
+	}
+	for {
+		// The new message allowed in the free space
+		if c.Config.Model.GetMaxToken() >= m.GetQtdTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+			c.RefreshTokenUsage()
+			break
+		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:] //Remove the oldest message
+		c.RefreshTokenUsage()
+	}
+	return nil
+}
+
+func (c *Chat) GetMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) CountMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) End() {
+	c.Status = "ended"
+}
+
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+	for m:= range c.Messages {
+		c.TokenUsage += c.Messages[m].GetQtdTokens()
+	}
 }
