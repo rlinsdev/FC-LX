226d8f18c66829390445ba2bad89d42b3e3a87de
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-18 09:59:24 -0300

    Coding model

--- ChatService/internal/domain/entity/model.go
+++ ChatService/internal/domain/entity/model.go
@@ -1,1 +1,21 @@
 package entity
+
+type Model struct {
+	Name string
+	MaxTokens int
+}
+
+func NewModel(name string, MaxTokens int) *Model {
+	return &Model {
+		Name: name,
+		MaxTokens: MaxTokens,
+	}
+}
+
+func (m *Model) GetMaxToken() int {
+	return m.MaxTokens
+}
+
+func (m *Model) GetModelName() string {
+	return m.Name
+}
