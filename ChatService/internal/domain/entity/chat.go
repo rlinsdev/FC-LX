8294fce899038a12a70a13f5ce0e657d24ce80de
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-18 10:15:53 -0300

    Chat configuration

--- ChatService/internal/domain/entity/chat.go
+++ ChatService/internal/domain/entity/chat.go
@@ -1,7 +1,14 @@
 package entity
 
 type Chatconfig struct {
-	Model *Model
+	Model            *Model
+	Temperature      float32  // Precision of answered
+	TopP             float32  // Conservative in words used
+	N                int      // 1 by default... Messages returned
+	Stop             []string // key to stop chat (reserved word)
+	MaxTokens        int      // number of token generated
+	PresencePenalty  float32  //
+	FrequencyPenalty float32  //
 }
 
 type Chat struct {
--- README.md
+++ README.md
@@ -9,6 +9,10 @@ $ go mod init github.com/rlinsdev/fclx/chatservice
 ```
 
 
-## Links
+## Links Docs
+* [OpenAI API docs](https://platform.openai.com/docs/api-reference/introduction)
+* [OpenAI](https://openai.com/)
+
+## Links Course
 * [FullCycle Page](https://fcexperience.fullcycle.com.br/evento/)
-* [FullCycle YouTube 1 ](https://www.youtube.com/watch?v=UugkE-OeE4E)
+* [FullCycle YouTube - 1](https://www.youtube.com/watch?v=UugkE-OeE4E)
