01cdb8fb4c4de45d154f42b6cf3ac34c4f762f9d
Rodrigo Lins <rlinsdev@gmail.com>
2023-04-18 10:03:41 -0300

    Chat structure

--- ChatService/internal/domain/entity/chat.go
+++ ChatService/internal/domain/entity/chat.go
@@ -1,1 +1,13 @@
-package entity 
+package entity
+
+type Chatconfig struct {
+	Model *Model
+}
+
+type Chat struct {
+	ID         string
+	UserID     string
+	Status     string
+	TokenUsage int
+	Config     *Chatconfig
+}
